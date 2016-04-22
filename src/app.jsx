c4c74383d4b487ed2957fe88171919473f2f5e73
Pete Taylour <pt9538@my.bristol.ac.uk>
2016-04-22 21:42:35 +0100

    basic hotloading react prototyping setup

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// require('basscss')
+
+
+const mockLines = [
+  "This is the first line of the commentary, it has different players in it."
+]
+
+const LatestCommentary = ({lines, time}) => {
+
+  return (
+    <div>
+      <header>
+        <h1>Live</h1>
+        <p>{time} min ago</p>
+      </header>
+      <div>
+        {lines[0]}
+      </div>
+      <div>
+        Footer
+      </div>
+    </div>
+  )
+}
+
+export default class App extends React.Component {
+  render() {
+    return <LatestCommentary lines={mockLines} time={1} />
+  }
+}
